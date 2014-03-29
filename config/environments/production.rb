2b99d0c7e069be94e2b6c7b85ca254009cc4f6f2
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-03-29 11:05:03 -0700

    precompiled assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,8 +20,7 @@ CoreValues::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
-
+  config.serve_static_assets = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
@@ -67,7 +66,7 @@ CoreValues::Application.configure do
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
-  config.i18n.fallbacks = true
+config.i18n.fallbacks = true
 
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
