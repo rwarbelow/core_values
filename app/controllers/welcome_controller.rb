92320681f4d55950ffe7cf9f33235c16f239b7c0
Rachel Warbelow <rachelwarbelow@gmail.com>
2014-03-30 15:38:42 -0700

    added redirect for users already logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
 	def index
-		
+		if current_user && current_user.user_type == "admin"
+			redirect_to admin_path
+		elsif current_user && current_user.user_type == "student"
+			redirect_to student_path
+		end
 	end
 end
